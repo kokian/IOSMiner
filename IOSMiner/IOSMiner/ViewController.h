b579d8edda3d99d121d184cf70411ec89c5bfd89
tortuga <kokian@tortugas-iMac.local>
2014-10-12 09:33:13 -0700

    Added Xcode template 'Single View Application'

---
+++ IOSMiner/IOSMiner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOSMiner
+//
+//  Created by tortuga on 12/10/14.
+//  Copyright (c) 2014 tortuga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IOSMiner/IOSMiner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IOSMiner
+//
+//  Created by tortuga on 12/10/14.
+//  Copyright (c) 2014 tortuga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
